2ba192ece25cf609cd58fb89c8f708dbc49160d5
Adin Ackerman <adinackerman@gmail.com>
2022-07-16 17:56:40 -0700

    Initial Commit

---
+++ GuidedCodeEditor/GuidedCodeEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidedCodeEditorApp.swift
+//  GuidedCodeEditor
+//
+//  Created by Adin Ackerman on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct GuidedCodeEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
